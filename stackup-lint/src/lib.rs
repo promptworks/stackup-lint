70f4080e03968bb979d24e4e917d9ba8a75af79a
Steve Sweetney <stevesweetney@gmail.com>
2019-08-23 13:52:39 -0400

    Add list of scalars supported by stackup

--- stackup-lint/src/lib.rs
+++ stackup-lint/src/lib.rs
@@ -4,9 +4,27 @@ use graphql_parser::{
     schema::{Definition, Field, ObjectType, TypeDefinition},
     Pos,
 };
+use lazy_static::lazy_static;
+use std::collections::HashSet;
 use std::error::Error;
 
 type Result<T> = ::std::result::Result<T, Box<dyn Error>>;
+
+lazy_static! {
+    static ref SCALARS: HashSet<String> = {
+        let mut ss = HashSet::new();
+        ss.insert("ID".to_string());
+        ss.insert("Boolean".to_string());
+        ss.insert("String".to_string());
+        ss.insert("Int".to_string());
+        ss.insert("Float".to_string());
+        ss.insert("Decimal".to_string());
+        ss.insert("Date".to_string());
+        ss.insert("DateTime".to_string());
+        ss.insert("File".to_string());
+        ss
+    };
+}
 
 pub fn check(schema: &str) -> Result<()> {
     let document =
