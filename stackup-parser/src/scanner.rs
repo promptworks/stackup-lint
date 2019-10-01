e2e899e2e712b63055f90ac6f86e4087c51a214c
Steve Sweetney <stevesweetney@gmail.com>
2019-10-01 12:04:23 -0400

    Add parser for punctuators - add combine crate

--- stackup-parser/src/scanner.rs
+++ stackup-parser/src/scanner.rs
@@ -1,3 +1,7 @@
+use combine::error::ParseError;
+use combine::stream::{state::SourcePosition, RangeStream};
+use combine::{self, choice, position, token, Parser};
+
 enum TokenType<'a> {
     Punctuator(Punctuator),
     Name(&'a str),
@@ -21,4 +25,66 @@ enum Punctuator {
     LeftCurlyBracket,
     RightCurlyBracket,
     VerticalBar,
+}
+
+struct Token<'a> {
+    kind: TokenType<'a>,
+    pos: SourcePosition,
+}
+
+impl<'a> Token<'a> {
+    pub fn new(kind: TokenType<'a>, pos: SourcePosition) -> Token<'a> {
+        Self { kind, pos }
+    }
+}
+
+fn punctuator<'a, I>() -> impl Parser<Input = I, Output = Token<'a>>
+where
+    I: RangeStream<Item = char, Range = &'a str, Position = SourcePosition>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    choice((
+        combine::char::string("...")
+            .with(position())
+            .map(|pos| Token::new(TokenType::Punctuator(Punctuator::Ellipsis), pos)),
+        token('!')
+            .with(position())
+            .map(|pos| Token::new(TokenType::Punctuator(Punctuator::Bang), pos)),
+        token('$')
+            .with(position())
+            .map(|pos| Token::new(TokenType::Punctuator(Punctuator::DollarSign), pos)),
+        token('&')
+            .with(position())
+            .map(|pos| Token::new(TokenType::Punctuator(Punctuator::Ampersand), pos)),
+        token('(')
+            .with(position())
+            .map(|pos| Token::new(TokenType::Punctuator(Punctuator::LeftParen), pos)),
+        token(')')
+            .with(position())
+            .map(|pos| Token::new(TokenType::Punctuator(Punctuator::RightParen), pos)),
+        token(':')
+            .with(position())
+            .map(|pos| Token::new(TokenType::Punctuator(Punctuator::Colon), pos)),
+        token('=')
+            .with(position())
+            .map(|pos| Token::new(TokenType::Punctuator(Punctuator::Equals), pos)),
+        token('@')
+            .with(position())
+            .map(|pos| Token::new(TokenType::Punctuator(Punctuator::AtSign), pos)),
+        token('[')
+            .with(position())
+            .map(|pos| Token::new(TokenType::Punctuator(Punctuator::LeftSquareBracket), pos)),
+        token(']')
+            .with(position())
+            .map(|pos| Token::new(TokenType::Punctuator(Punctuator::RightSquareBracket), pos)),
+        token('{')
+            .with(position())
+            .map(|pos| Token::new(TokenType::Punctuator(Punctuator::LeftCurlyBracket), pos)),
+        token('}')
+            .with(position())
+            .map(|pos| Token::new(TokenType::Punctuator(Punctuator::RightCurlyBracket), pos)),
+        token('|')
+            .with(position())
+            .map(|pos| Token::new(TokenType::Punctuator(Punctuator::VerticalBar), pos)),
+    ))
 }
